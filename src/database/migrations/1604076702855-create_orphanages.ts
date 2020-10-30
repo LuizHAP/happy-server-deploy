337996709e7b0d52394525926986cc96f4bc0abf
Luiz Henrique <lpansarini@gmail.com>
2020-10-30 15:11:43 -0300

    create orphanage on post

--- src/database/migrations/1604076702855-create_orphanages.ts
+++ src/database/migrations/1604076702855-create_orphanages.ts
@@ -41,6 +41,10 @@ export class createOrphanages1604076702855 implements MigrationInterface {
           type: 'text',
         },
         {
+          name: 'opening_hours',
+          type: 'varchar',
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false,
