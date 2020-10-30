a8d66b5c8a1924f80ddf3185bd5f59851e3bf5e0
Luiz Henrique <lpansarini@gmail.com>
2020-10-30 15:14:52 -0300

    routes changes using routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,14 @@
 import express from 'express'
 import cors from 'cors';
 
-import { getRepository } from 'typeorm'
-import Orphanage from './models/Orphanage'
-
 import './database/connection'
+
+import routes from './routes'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello' })
-})
-
-app.post('/orphanages', async (request, response) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = request.body;
-
-    const orphanagesRepository = getRepository(Orphanage);
-
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    });
-
-    await orphanagesRepository.save(orphanage);
-    
-    return response.json({ message: 'Hello' })
-})
+app.use(routes);
 
 app.listen(3333);
